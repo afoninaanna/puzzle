7ba28ab939b2f23ebc040c893101d6499068811e
afoninaanna <afoninaa903@gmail.com>
2022-12-18 22:17:30 +0400

    feat: add firebase database

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase} from "firebase/database";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAzVmhTFrayosAzTY3ImdkIuyrhX86V2Po",
@@ -9,9 +10,10 @@ const firebaseConfig = {
     storageBucket: "puzzle-8a8e3.appspot.com",
     messagingSenderId: "613998161873",
     appId: "1:613998161873:web:be525f7eaa901ac9887af0",
-    measurementId: "G-P5613C75T4"
+    measurementId: "G-P5613C75T4",
+    databaseURL: 'https://puzzle-8a8e3-default-rtdb.firebaseio.com'
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const database = getDatabase(app);
