49e290e67d04563a5fdfe3d10c75457effd0a459
zhuzhavladislav <zhuzha_vladislav@mail.ru>
2022-12-26 03:23:44 +0400

    add: help page

--- src/components/Info/Developers/Developers.jsx
+++ src/components/Info/Developers/Developers.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import s from "./style.module.css";
 import { HELP_ROUTE } from "../../../utils/consts";
-import { Link } from "react-router-dom";
 
 const Developers = () => {
   return (
     <div className={s.Page}>
       <div className={s.Container}>
-        <h1>Сведения о разработчиках:</h1>
-        <p>Курсовая работа по проекту "Программная инженерия"</p>
-        <p>Студентов группы 6415 Афониной Анны и Жужи Владислава</p>
-        <p>2022 год</p>
-        <Link to={HELP_ROUTE}>
+        <p>Самарский университет</p>
+        <p>Кафедра программных систем</p>
+        <p style={{ marginTop: 30 }}>
+          Курсовой проект по дисциплине Программная инженерия
+        </p>
+        <p style={{ marginTop: 30 }}>
+          Тема проекта: «Автоматизированная система «Игра
+        </p>
+        <p>Puzzle» с функциями администратора»</p>
+        <p style={{ marginTop: 30 }}>
+          Разработчки: студенты группы 6415-020302D
+        </p>
+        <p style={{ alignSelf: "right" }}>Жужа Владислав Сергеевич</p>
+        <p style={{ alignSelf: "right" }}>Афонина Анна Александровна</p>
+        <p style={{ marginTop: 30 }}>Самара 2022</p>
+        {/* <Link to={HELP_ROUTE}>
           <button className={s.Button}>Справка о системе</button>
-        </Link>
+        </Link> */}
+        <a href="./help/help.html" target="_blank">
+          <button className={s.Button}>Справка о системе</button>
+        </a>
+        {/* <button className={s.Button}>
+          Справка о системе
+        </button> */}
       </div>
     </div>
   );
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -25,9 +25,9 @@ const Login = () => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="#000000"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <circle cx="12" cy="12" r="10" />
             <path d="M9.09 9a3 3 0 015.83 1c0 2-3 3-3 3" />
