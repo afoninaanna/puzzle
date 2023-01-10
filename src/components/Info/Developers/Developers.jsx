5870529d77c00c0de883090f09cb7ff5044b98ef
afoninaanna <afoninaa903@gmail.com>
2023-01-10 21:27:50 +0400

    add: проверка наличия файла справки

--- src/components/Info/Developers/Developers.jsx
+++ src/components/Info/Developers/Developers.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./style.module.css";
 import { HELP_ROUTE } from "../../../utils/consts";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const Developers = () => {
-
+  const storage = getStorage();
+  const [urlHelpFile, setUrlHelpFile] = useState('')
   // const tryRequire = async () => {
   //     try {
   //       let response = await fetch("http://localhost:3000/help/help.html");
@@ -14,7 +16,10 @@ const Developers = () => {
   //       console.log("Fetch error:" + err);
   //     }
   // };
-
+  // const helpFileRef = ref(storage, `https://firebasestorage.googleapis.com/v0/b/puzzle-8a8e3.appspot.com/o/help%2Fhelp.html?alt=media&token=309effbc-5bca-48ba-acb2-c5991cfde7b6`);
+  // console.log(helpFileRef);
+  
+  getDownloadURL(ref(storage, 'help/help.html')).then((url) => setUrlHelpFile(url)).catch((error) => alert("Файл справки не найден", error.code));
   return (
     <div className={s.Page}>
       <div className={s.Container}>
@@ -36,7 +41,8 @@ const Developers = () => {
         {/* <Link to={HELP_ROUTE}>
           <button className={s.Button}>Справка о системе</button>
         </Link> */}
-        <a href="./help/help.html" target="_blank">
+        
+        <a href={urlHelpFile} target="_blank">
           <button className={s.Button}>Справка о системе</button>
         </a>
         {/* <button className={s.Button} onClick={tryRequire}>
